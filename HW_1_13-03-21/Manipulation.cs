4eafce1916704ac519c275dadad701a38073667a
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-03-14 21:10:41 +0500

    Добавьте файлы проекта.

---
+++ HW_1_13-03-21/Manipulation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HW_1_13_03_21
+{
+    class Manipulation
+    {
+        public int[] DoReverse(int[] array) => array.Reverse().ToArray();
+        public string[] FormatByInc(string[] array) => array.OrderBy(t => t.Length).ToArray();
+        public int[] SumAndCount(int[] arrays)
+        {
+            int count = arrays.Where(t => t > 0).Count();
+            int sum = arrays.Where(t => t < 0).Sum();
+            int[] arr = { count, sum };
+            return arr;
+        }
+        public int[] GetUniqNumber(int[] array)
+        {
+            return (array.GroupBy(i => i).Where(i => i.Count() == 1).Select(i => i.Key)).ToArray();
+        }
+    }
+}
---
+++ HW_1_13-03-21/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace HW_1_13_03_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            Manipulation manipulation = new Manipulation();
+            int[] newArray = manipulation.DoReverse(numbers);
+            foreach (var item in newArray) Console.Write($"{item} ");
+
+            Console.WriteLine();
+            string[] devices = { "Telescopes", "Glasses", "Eyes", "Monocles" };
+            string[] newArraySort = manipulation.FormatByInc(devices);
+            foreach (var item in newArraySort) Console.Write($"{item} ");
+
+            Console.Write("\n[ ");
+            int[] digits = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 };
+            foreach (var item in manipulation.SumAndCount(digits)) Console.Write(item + " ");
+            Console.WriteLine(" ]");
+
+            int[] dig = { 1, 1, 1, 1, 2, 1, 1 };
+            int[] nu = manipulation.GetUniqNumber(dig);
+            foreach (var item in nu)Console.WriteLine(item);
+        }
+    }
+}
